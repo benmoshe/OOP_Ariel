408e80f28e2e12a22e28f24d3f77168b6a1bb589
boazbenmoshe <benmo@g.ariel.ac.il>
2019-11-30 21:55:02 +0200

    Minor additional documentation regarding the log file default name.

--- Class5/src/Test_101.java
+++
@@ -1,9 +0,0 @@
-import model.Address;
-
-public class Test_101 {
-    public static void main(String[] a) {
-        Address a = new Address();
-
-        System.out.println("123 "+a);
-    }
-}
---
+++ Class6/src/Ex1/function.java
@@ -0,0 +1,16 @@
+package Ex1;
+
+import java.io.Serializable;
+
+/** This interface represents a simple function of type y=f(x), where both y and x are real numbers.
+**/
+public interface function extends Serializable{
+	public double f(double x);
+	/** 
+	 * return a String representing this complex function
+	 */
+	public String toString();
+	public function initFromString(String s);
+	public function copy(); // clone
+	public boolean equals(Object obj);
+}
